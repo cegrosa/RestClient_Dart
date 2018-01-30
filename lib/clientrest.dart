67a89db08399b9135a74c0f8a85284afbfa5bd87
cegrosa <vicentejodar97@gmail.com>
2018-01-30 23:31:35 +0100

    Small changes

--- lib/clientrest.dart
+++ lib/clientrest.dart
@@ -4,10 +4,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class RestClient {
-  var httpClient = new HttpClient();
+  var httpClient;
 
   //Get object from specific id
   Future<Map> getObject(Uri url) async {
+    httpClient = new HttpClient();
     try {
       var request = await httpClient.getUrl(url);
       var response = await request.close();
@@ -21,6 +22,7 @@ class RestClient {
 
   //Get objects List from specific url
   Future<List> getList(Uri url) async {
+    httpClient = new HttpClient();
     try {
       var request = await httpClient.getUrl(url);
       var response = await request.close();
@@ -33,9 +35,10 @@ class RestClient {
     }
   }
 
-  //Post object
+  //Post object to specific url
   post(Uri url, Map data) async {
     try {
+      //Remove field 'id' from Map
       data.remove("id");
 
       var response = await http.post(url,

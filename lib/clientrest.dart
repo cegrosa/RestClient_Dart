43e6508dc5432b3bc95f82f5c90dc8effa31d732
cegrosa <vicentejodar97@gmail.com>
2018-01-31 23:22:49 +0100

    Added 'Put' and 'Delete' methods, too method that return parsed uri

--- lib/clientrest.dart
+++ lib/clientrest.dart
@@ -4,55 +4,66 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class RestClient {
-  var httpClient;
+  var httpClient = new HttpClient();
 
-  //Get object from specific id
-  Future<Map> getObject(Uri url) async {
-    httpClient = new HttpClient();
-    try {
-      var request = await httpClient.getUrl(url);
-      var response = await request.close();
-      var responseBody = await response.transform(UTF8.decoder).join();
-      return JSON.decode(responseBody);
-    } catch (exception, stackTrace) {
-      print(exception);
-      print(stackTrace);
-    }
+  // Get object from specific id
+  Future<Map> getObject(String url) async {
+    Uri uri = _parseUriStr(url);
+    var request = await httpClient.getUrl(uri);
+    var response = await request.close();
+    var responseBody = await response.transform(UTF8.decoder).join();
+
+    return JSON.decode(responseBody);
   }
 
-  //Get objects List from specific url
-  Future<List> getList(Uri url) async {
-    httpClient = new HttpClient();
-    try {
-      var request = await httpClient.getUrl(url);
-      var response = await request.close();
-      var responseBody = await response.transform(UTF8.decoder).join();
-      print("AAA: " + JSON.decode(responseBody).toString());
-      return JSON.decode(responseBody);
-    } catch (exception, stackTrace) {
-      print(exception);
-      print(stackTrace);
-    }
+  // Get objects List from specific url
+  Future<List> getList(String url) async {
+    Uri uri = _parseUriStr(url);
+    var request = await httpClient.getUrl(uri);
+    var response = await request.close();
+    var responseBody = await response.transform(UTF8.decoder).join();
+
+    return JSON.decode(responseBody);
   }
 
-  //Post object to specific url
-  post(Uri url, Map data) async {
-    try {
-      //Remove field 'id' from Map
-      data.remove("id");
+  // Post object to specific url
+  post(String url, Map data) async {
+    Uri uri = _parseUriStr(url);
 
-      var response = await http.post(url,
-          headers: {'content-type': 'application/json'},
-          body: JSON.encode(data));
-      print("Response status: ${response.statusCode}");
-      print("Response body: ${response.body}");
-    } catch (exception, stackTrace) {
-      print(exception);
-      print(stackTrace);
-    }
+    // Remove field 'id' from Map
+    data.remove("id");
+
+    var response = await http.post(uri,
+        headers: {'content-type': 'application/json'}, body: JSON.encode(data));
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
   }
 
-  _put(Uri url) async {}
+  // Put/Update object to specific url
+  // If url pass 'id' (E.g.: 'localhost/kittie/1'),
+  //  this method act like post()
+  put(String url, Map data) async {
+    Uri uri = _parseUriStr(url);
 
-  _delete(Uri url) async {}
+    var response = await http.put(uri,
+        headers: {'content-type': 'application/json'}, body: JSON.encode(data));
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
+  }
+
+  // Delete object from specific url
+  delete(String url) async {
+    Uri uri = _parseUriStr(url);
+
+    var request = await httpClient.deleteUrl(uri);
+    var response = await request.close();
+    var responseBody = await response.transform(UTF8.decoder).join();
+
+    return JSON.decode(responseBody);
+  }
+
+  // Return parsed Uri
+  Uri _parseUriStr(String url){
+    return Uri.parse(url);
+  }
 }
